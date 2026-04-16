3eb3f25e6aa9af9223825725aa2b0deb3188a24d
Fraxoo <fraxo.officiel@gmail.com>
2026-04-16 10:16:05 +0200

    added login / register

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,37 +3,58 @@
 namespace App\Form;
 
 use App\Entity\User;
-use Doctrine\Common\Collections\Placeholder;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Unique;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', null , [
+            ->add('email', null, [
                 'attr' => [
                     'placeholder' => 'votre@email.com'
+                ],
+                'constraints' => [
+                    new Email(
+                        message: "Veuillez entrez une email valide"
+                    ),
+                    new NotBlank(
+                        message: "Veuillez entrez une email"
+                    )
                 ]
             ])
-            ->add('username', null , [
+            ->add('username', null, [
                 'attr' => [
                     'placeholder' => 'nom_utilisateur'
+                ],
+                'constraints' => [
+                    new NotBlank(
+                        message: "Veuillez entrez un Nom d'utilisateur"
+                    ),
+                    new Length(
+                        min: 2,
+                        minMessage: 'Your username should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        max: 4096,
+                    ),
+                    new Unique(
+                        message: "Nom d'utilisateur déja pris"
+                    )
                 ]
             ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password' , 
+                'attr' => [
+                    'autocomplete' => 'new-password',
                     'placeholder' => "mot de passe"
                 ],
                 'constraints' => [

1a7a6284622978ac62a37ab00ca175c475a9135d
Fraxoo <fraxo.officiel@gmail.com>
2026-04-21 11:57:31 +0200

    added checkbox

--- src/Twig/Components/TaskComponent.php
+++ src/Twig/Components/TaskComponent.php
@@ -5,6 +5,9 @@ namespace App\Twig\Components;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 use App\Entity\Task as TaskEntity;
+use App\Enum\Status;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 
 #[AsLiveComponent]
@@ -12,19 +15,37 @@ final class TaskComponent
 {
     use DefaultActionTrait;
 
-    
+    // function __construct()
+    // {
+    //     throw new \Exception('Not implemented');
+    // }
 
-    #[LiveProp(writable:true)]
+    #[LiveProp(writable: true)]
     public TaskEntity $task;
-    // public TaskEntity $task;
-    
-    #[LiveProp(writable:true)]
+
+    #[LiveProp(writable: true)]
     public bool $isChecked = false;
 
-    public function getTask() {
-        $this->task->setTitle($this->isChecked ?"c'est vrai" : "c'est faux");
+    private EntityManagerInterface $em;
 
-        return $this->task;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
 
+
+    public function mount(TaskEntity $task): void
+    {
+        $this->task = $task;
+        $this->isChecked = $task->getStatus() === Status::completed;
+    }
+
+    #[LiveAction]
+    public function saveChecked(): void
+    {
+        $this->task->setStatus($this->isChecked ? Status::completed : Status::pending);
+        $this->em->flush();
+
+        $this->isChecked = $this->task->getStatus() === Status::completed;
     }
 }
